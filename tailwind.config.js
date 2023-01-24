22cfc01c1373c27f2d807848b0f108774616bdec
Wikuu <a4m4ttjob@gmail.com>
2023-01-24 20:04:29 +0300

    hero section and counters sections completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
         yellow: '#ffce4e',
         purple: '#1c121f',
         'trans-white': 'rgba(255, 255, 255, 0.01)',
-        'slightly-trans-white': 'rgba(255, 255, 255, 0.1)',
+        'slightly-trans-white': 'rgba(255, 255, 255, 0.55)',
+        'box-shadow-trans-white': 'rgba(0 0 0 / 10%)',
       },
       fontFamily: {
         'work-sans': ['Work Sans', 'sans-serif'],
         oxanium: ['Oxanium', 'cursive'],
+      },
+      height: {
+        'nft-art-height': '300px',
       },
     },
   },
